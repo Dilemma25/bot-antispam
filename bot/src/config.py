6d94fd7292358a5a839847ac022b4e7476dcc059
Dilemma25 <valtert90@mail.ru>
2025-05-30 20:12:22 +1000

    wip

--- bot/src/config.py
+++ bot/src/config.py
@@ -6,14 +6,18 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from middlewares.sibscription_middleware import SibscriptionMiddleware
 from handlers.find_location_dev import debug_router
+import redis.asyncio as redis
+
+
+redis_client = redis.Redis(host='redis', port=6379, db=0)
 
 load_dotenv()
 TOKEN = getenv("BOT_TOKEN")
+CHANNEL_ID = getenv('CHANNEL_ID')
 
 dp = Dispatcher()
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-CHANNEL_ID = getenv('CHANNEL_ID')
 dp.message.middleware(SibscriptionMiddleware(bot=bot, channel_id=CHANNEL_ID))
 
 
--- bot/src/main.py
+++ bot/src/main.py
@@ -1,27 +1,18 @@
 import asyncio
 import logging
 import sys
-from aiogram.filters import CommandStart
-from aiogram import types
-
 from config import dp
 from config import bot
+from config import redis_client
 
-# @dp.message(CommandStart())
-# async def command_start_handler(message: Message) -> None:
-#     """
-#     This handler receives messages with `/start` command
-#     """
-#     await message.answer(f"Hello, {message.from_user.full_name}!")
-
-# @dp.message()
-# async def debug_all_messages(message: types.Message):
-#     logging.info(f"Получено сообщение: type={message.content_type}, from={message.from_user.full_name}, text={message.text}")
 
 async def main() -> None:
+    await redis_client.set("my-key", "value")
+    value = await redis_client.get("my-key")
+    print(value)
     await dp.start_polling(bot)
 
-
+    
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
